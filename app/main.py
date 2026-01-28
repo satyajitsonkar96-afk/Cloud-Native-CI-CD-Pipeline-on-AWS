ce4d6095d9594973f03ebee3f22aa48aca2b98f7
satyajit sonkar <satyajitsonkar96@gmail.com>
2026-01-28 14:41:54 +0000

    change

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return " Your Python application is running successfully!"
+    return " Application is running successfully!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
